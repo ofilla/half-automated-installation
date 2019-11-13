4348b0422e2264e64543791dfd8c796c1bc4a1b2
Oliver Filla <ocl@hotmail.de>
2019-11-13 15:14:42 +0100

    Add modules for hai and test
    tox works

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Fully Automated Setup',
+      version='0.1',
+      description='Set up a fresh installed computer',
+      author='Oliver Filla',
+      author_email='ocl@hotmail.de',
+      url='https://github.com/ofilla/half-automated-installation',
+      packages=['hai'],
+     )
