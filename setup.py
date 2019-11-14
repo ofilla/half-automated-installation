0cd54aec2f444f7e082a2b6558a20d45c1acb7ee
Oliver Filla <ocl@hotmail.de>
2019-11-14 22:36:28 +0100

    Update tox.ini: also test on PEP8

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='Fully Automated Setup',
-      version='0.1',
-      description='Set up a fresh installed computer',
-      author='Oliver Filla',
-      author_email='ocl@hotmail.de',
-      url='https://github.com/ofilla/half-automated-installation',
-      packages=['hai'],
-     )
+setup(
+    name='Fully Automated Setup',
+    version='0.1',
+    description='Set up a fresh installed computer',
+    author='Oliver Filla',
+    author_email='ocl@hotmail.de',
+    url='https://github.com/ofilla/half-automated-installation',
+    packages=['hai'],
+)
