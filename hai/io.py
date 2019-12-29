56cd56427fefff4986e01521b3dee9e9c9b94309
Oliver Filla <ocl@hotmail.de>
2019-12-29 21:42:26 +0100

    Read config files

--- hai/io.py
+++ hai/io.py
@@ -2,15 +2,22 @@
 IO methods
 '''
 import os
+from configparser import ConfigParser
+
+TEST_FILENAME = "test_file.towrite"
 
 
 def path_in_dir(directory, filename):
-    "return path of file in directory"
+    """ (str, str) -> str
+    Return path of file in directory
+    """
     return directory + "/" + filename
 
 
 def make_parent_dir(filename):
-    "Create parent directories for file <filename>."
+    """ (str) -> None
+    Create parent directories for file <filename>.
+    """
     path = ""
     for dirname in filename.split("/"):
         dirpath = path + dirname
@@ -20,22 +27,41 @@ def make_parent_dir(filename):
 
 
 def is_existing_dir(dirname):
-    "Test if a given path is an existing directory."
+    """ (str) -> boolean
+    Test if a given path is an existing directory.
+    """
     return os.path.isdir(dirname)
 
 
 def remove_empty_dir(dirname):
-    "Delete empty directory, if existing."
+    """ (str) -> None
+    Delete empty directory, if existing.
+    """
     if is_existing_dir(dirname):
         os.rmdir(dirname)
 
 
 def remove_file(filename):
-    "Delete empty directory, if existing."
+    """ (str) -> None
+    Delete empty directory, if existing.
+    """
     if is_existing_file(filename):
         os.remove(filename)
 
 
 def is_existing_file(filename):
-    "Test if a given path is an existing file."
+    """ (str) -> None
+    Test if a given path is an existing file.
+    """
     return os.path.isfile(filename)
+
+
+def read_config(filename):
+    """ (str) -> dict
+    Read a config file and return lines as dict.
+    """
+    config = ConfigParser()
+    content = "[default]\n"
+    content += open(filename).read()
+    config.read_string(content, filename)
+    return config['default']
