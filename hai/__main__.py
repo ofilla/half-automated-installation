d5652e49f664045d77264e24108dd931f6907f1f
Oliver Filla <ocl@hotmail.de>
2020-01-03 13:22:26 +0100

    Remove obsolete Error class

--- hai/__main__.py
+++ hai/__main__.py
@@ -28,19 +28,6 @@ __version__ = 0.1
 __date__ = '2019-11-17'
 
 
-class CLIError(Exception):
-    """Generic exception to raise and log different fatal errors."""
-    def __init__(self, msg):
-        super(CLIError).__init__(type(self))
-        self.msg = "E: %s" % msg
-
-    def __str__(self):
-        return self.msg
-
-    def __unicode__(self):
-        return self.msg
-
-
 def main(argv=None):
     """Command line options."""
 
